24bdf4a4c187ad3c6df4879ebef64926db8df6cd
Mark Kuo <starryalley@gmail.com>
2020-02-03 22:03:34 +1100

    add Chinese text as font

---
+++ chinese_fonts/build_font.sh
@@ -0,0 +1,7 @@
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources/fonts/font -r 30
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources-round-218x218/fonts/font -r 30
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources-round-240x240/fonts/font -r 32
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources-round-260x260/fonts/font -r 35
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources-round-280x280/fonts/font -r 40
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png [1-9][0-9].png -o ../resources-round-390x390/fonts/font -r 54
+
